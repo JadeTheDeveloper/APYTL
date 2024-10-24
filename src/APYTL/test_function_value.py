250b23f4925fbeb3415135afe4425a05385da388
JadeTheDeveloper <137838669+JadeTheDeveloper@users.noreply.github.com>
2024-10-24 11:15:07 -0600

    Added the option choose weather to quit after the test completes

--- src/APYTL/test_function_value.py
+++ src/APYTL/test_function_value.py
@@ -5,13 +5,16 @@ from colorama import Fore
     params:
     function_to_compare: The fucntion you're testing the output of(This value can or cannot be NULL)
     excpected_value: The value that you are expecting the function to output
+    exit_after_result: ether true or false, determines wheather you want to exit after the test completes
 """
-def test_function_value(function_to_compare, expected_value):
+def test_function_value(function_to_compare, expected_value, exit_after_result):
     if(function_to_compare != expected_value):
         print(Fore.RED + 'The function did not return its expected value')
         print(Fore.RED + 'Expected: ' + expected_value)
         print(Fore.RED + 'Function Returned: ' + function_to_compare)
-        exit(1)
+        if(exit_after_result == True):
+            exit(1)
     if(function_to_compare == expected_value):
         print(Fore.CYAN + 'The function returned its expected value!')
-        exit(0)
+        if(exit_after_result == True):
+            exit(0)
