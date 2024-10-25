8db75f9779e08de53b5dd4f29b3209107921acc6
JadeTheDeveloper <137838669+JadeTheDeveloper@users.noreply.github.com>
2024-10-25 08:22:40 -0600

    Created basic implementation of test_function_and_write for testing

--- src/APYTL/test_function_and_write.py
+++ src/APYTL/test_function_and_write.py
@@ -1,2 +1,8 @@
 def test_function_and_write(function_to_compare, expected_value):
-    pass
+    f = open("testresults.log")
+    if(function_to_compare == expected_value):
+        f.write("Expected: " + str(function_to_compare) + '\nGot: ' + str(expected_value) + '\nTest Passed!')
+        f.close()
+    else:
+        f.write("Expected: " + str(function_to_compare) + '\nGot: ' + str(expected_value) + '\nTest Failed!')
+        f.close()
