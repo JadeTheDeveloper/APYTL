d392630a6b9f218c6488877eb65c8de765b68d31
JadeTheDeveloper <cazextra@gmail.com>
2024-10-25 20:58:04 -0600

    Debugged and properly implemented the features added in commit 8db75f9

--- src/APYTL/test_function_and_write.py
+++ src/APYTL/test_function_and_write.py
@@ -5,7 +5,7 @@
     filepath: the file that the completed test will output to, leave as NULL or blank to use the default file(testresults.log)
 """
 def test_function_and_write(function_to_compare, expected_value, filepath):
-    f = open("testresults.log")
+    f = open("testresults.log", "a")
     if(function_to_compare == expected_value):
         f.write("Expected: " + str(function_to_compare) + '\nGot: ' + str(expected_value) + '\nTest Passed!')
         f.close()
